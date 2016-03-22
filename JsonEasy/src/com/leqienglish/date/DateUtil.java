967fc3819983716fcaabee319146d42bbbfab6fb
zhuqing <25zq@163.com>
2016-03-22 22:40:35 +0800

    字符到日期的转换
    字符到日期的转换

--- JsonEasy/src/com/leqienglish/date/DateUtil.java
+++ JsonEasy/src/com/leqienglish/date/DateUtil.java
@@ -5,6 +5,7 @@
  */
 package com.leqienglish.date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,30 +15,53 @@ import java.util.Date;
  * @author zhuleqi
  */
 public class DateUtil {
+
     private static SimpleDateFormat dateFormater = new SimpleDateFormat();
-    
-    public static String toDateFormat(Object date , String format){
-        if(date ==null || format ==null){
+
+    public synchronized static String toDateFormat(Object date, String format) {
+        if (date == null || format == null) {
             return "";
         }
         dateFormater.applyPattern(format);
-        if(date instanceof Date){
-            return toDateFormat((Date)date , dateFormater);
-        }else if(date instanceof Calendar){
-              return toDateFormat((Calendar)date , dateFormater);
+        if (date instanceof Date) {
+            return toDateFormat((Date) date, dateFormater);
+        } else if (date instanceof Calendar) {
+            return toDateFormat((Calendar) date, dateFormater);
         }
-        
+
         return "";
-        
+
     }
-    
-    private static String toDateFormat(Date date , SimpleDateFormat dateFormater){
-        
-      return  dateFormater.format(date);
-        
+
+    private static String toDateFormat(Date date, SimpleDateFormat dateFormater) {
+
+        return dateFormater.format(date);
+
     }
-    
-    private static String toDateFormat(Calendar date , SimpleDateFormat dateFormater){
-         return  dateFormater.format(date.getTime());
+
+    private static String toDateFormat(Calendar date, SimpleDateFormat dateFormater) {
+        return dateFormater.format(date.getTime());
+    }
+
+    private static Date toDate(String date, SimpleDateFormat dateFormater) throws ParseException {
+        return dateFormater.parse(date);
+    }
+
+    public synchronized static Object toDate(String date, String format, Class claz) throws ParseException {
+        if (date == null || format == null || claz == null) {
+            return null;
+        }
+        dateFormater.applyPattern(format);
+        if (claz.getSimpleName().equals("Date")) {
+
+            return toDate(date, dateFormater);
+        } else if (claz.getSimpleName().equals("Calendar")) {
+            Date dateD = toDate(date, dateFormater);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(dateD);
+            return calendar;
+        }
+
+        return null;
     }
 }
